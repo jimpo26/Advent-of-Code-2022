329e1d175a72beb4777d1c7bfc0a49c98c021f4a
jimpo26 <loryparadise04@gmail.com>
2022-12-03 11:20:53 +0100

    Day three solutions

--- day3/part1.py
+++ day3/part1.py
@@ -1,6 +1,6 @@
 with open('input.txt', 'r') as f:
     data = f.read().splitlines()
-
+from datetime import datetime
 PRIORITIES = '-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # there is an - in front of the letters to have the correct priority number without sum 1 each time
 
@@ -10,10 +10,9 @@ for rucksack in data:
 
     compartments = [rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]]
 
-    compartments[0] = ''.join((set(compartments[0])))  # remove duplicates
-
     for i in range(len(compartments[0])):
         if compartments[0][i] in compartments[1]:
             priorities_sum += PRIORITIES.index(compartments[0][i])
+            break
 
 print(priorities_sum)
